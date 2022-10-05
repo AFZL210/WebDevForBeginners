49c5e0ace08832417a107fe69075ceb940d81e2b
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2022-10-05 11:58:02 +0530

    Add files via upload

---
+++ AsyncAwait | Callback | Promises/Promise.js
@@ -0,0 +1,46 @@
+const datas = [
+    {
+        name: "Afzal",
+        prof: "Student",
+        age: 18
+    },
+
+    {
+        name: "Raju",
+        prof: "Student",
+        age: 18
+    }
+]
+
+
+function getDatas() {
+    setTimeout(() => {
+        let output = "";
+        datas.forEach((data,index) => {
+            output+=`<li>${data.name}</li>`;
+        })
+
+        document.body.innerHTML = output;
+    },1000)
+}
+
+function createData(newData) {
+
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            datas.push(newData);
+            let error = false;
+            if( !error ) {
+                resolve();
+            }else{
+                reject("Error");
+            }
+        },2000)
+    })
+
+}
+
+newUser = {name:"vivek",prof:"student",age:20};
+
+
+createData(newUser).then(getDatas)
