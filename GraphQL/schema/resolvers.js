0f2d27409d8e5c0352357b8e15940a77a2ff9214
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-04-07 19:00:54 +0530

    GraphQL basic query and mutations

--- GraphQL/schema/resolvers.js
+++ GraphQL/schema/resolvers.js
@@ -1,9 +1,61 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
     Query: {
-        users() {
+        users: () => {
             return UserList;
+        },
+
+        user: (parent, args) => {
+            const id = args.id;
+            const user = _.find(UserList, {id: Number(id)});
+            return user;
+        },
+
+        movies: () => {
+            return MovieList;
+        },
+
+        movie: (parent, args) => {
+            const name = args.name;
+            return _.find(MovieList, { name })
+        }
+    },
+
+    // resolver for a type
+    User: {
+        favoriteMovies: () => {
+            return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010)
+        }
+    },
+
+    // mutations
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input;
+            const lastId = UserList[UserList.length-1].id;
+            user.id = lastId;
+            UserList.push(user);
+            return user;
+        },
+
+        updateUsername: (parent, args) => {
+            let { id, newusername } = args.input;
+            id = parseInt(id);
+            UserList.forEach((user) => {
+                if(user.id === id) {
+                    user.username = newusername;
+                    newUser = user;
+                }
+            })
+            return newUser;
+        },
+
+        deleteUser: (parent, args) => {
+            let id = args.id;
+            _.remove(UserList, (user) => user.id == Number(id))
+            return null;
         }
     }
 }
--- GraphQL/schema/type-defs.js
+++ GraphQL/schema/type-defs.js
@@ -7,11 +7,49 @@ const typeDefs = gql`
         name: String!
         username: String!
         age: Int!
-        nationality: String!
+        nationality: Nationality
+        friends: [User]
+        favoriteMovies: [Movie]
+    }
+
+    type Movie {
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
     }
 
     type Query {
         users: [User!]!
+        user(id: ID!): User
+        movies: [Movie]!
+        movie(name: String!): Movie
+    }
+
+    input createUserInput {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = INDIA
+    }
+
+    input updateUsernameInput {
+        id: ID!
+        newusername: String!
+    }
+
+    type Mutation {
+        createUser(input: createUserInput!): User
+        updateUsername(input: updateUsernameInput): User
+        deleteUser(id: ID!): User
+    }
+
+    enum Nationality {
+         INDIA
+         BRAZIL
+         CANADA
+         GERMANY
+         CHILE
     }
 `
 
